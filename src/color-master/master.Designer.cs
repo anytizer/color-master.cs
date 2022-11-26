559743d7f46ddb5f3ae975341b594721704c1daf
Bimal Poudel <anytizer@users.noreply.github.com>
2022-11-26 12:17:54 -0700

    publish settings for single file

--- src/color-master/master.Designer.cs
+++ src/color-master/master.Designer.cs
@@ -41,14 +41,27 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.applicationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.alwaysOnTopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cSSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyColorCodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyFullCSSToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.projectSourceCodeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.anytizerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button6 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // trackBar1
             // 
+            this.trackBar1.AutoSize = false;
             this.trackBar1.Location = new System.Drawing.Point(84, 300);
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(609, 45);
@@ -57,6 +70,7 @@
             // 
             // trackBar2
             // 
+            this.trackBar2.AutoSize = false;
             this.trackBar2.Location = new System.Drawing.Point(84, 351);
             this.trackBar2.Name = "trackBar2";
             this.trackBar2.Size = new System.Drawing.Size(609, 45);
@@ -65,6 +79,7 @@
             // 
             // trackBar3
             // 
+            this.trackBar3.AutoSize = false;
             this.trackBar3.Location = new System.Drawing.Point(84, 402);
             this.trackBar3.Name = "trackBar3";
             this.trackBar3.Size = new System.Drawing.Size(609, 45);
@@ -73,9 +88,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 300);
+            this.button1.Location = new System.Drawing.Point(10, 300);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 45);
+            this.button1.Size = new System.Drawing.Size(68, 45);
             this.button1.TabIndex = 7;
             this.button1.Text = "R";
             this.button1.UseVisualStyleBackColor = true;
@@ -93,7 +108,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(11, 402);
+            this.button3.Location = new System.Drawing.Point(12, 402);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(67, 45);
             this.button3.TabIndex = 9;
@@ -105,10 +120,11 @@
             // 
             this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
             this.button4.FlatAppearance.BorderSize = 0;
-            this.button4.Location = new System.Drawing.Point(12, 12);
+            this.button4.Location = new System.Drawing.Point(12, 27);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(681, 182);
+            this.button4.Size = new System.Drawing.Size(681, 166);
             this.button4.TabIndex = 10;
+            this.button4.Text = "Background";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -116,7 +132,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(397, 208);
+            this.label1.Location = new System.Drawing.Point(388, 202);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(294, 89);
             this.label1.TabIndex = 11;
@@ -129,16 +145,17 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(180, 180);
             this.button5.TabIndex = 12;
+            this.button5.Text = "Profile";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(270, 229);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(285, 240);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 65);
+            this.label2.Size = new System.Drawing.Size(41, 51);
             this.label2.TabIndex = 13;
             this.label2.Text = "«";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -146,17 +163,17 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(337, 229);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(332, 240);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 65);
+            this.label3.Size = new System.Drawing.Size(41, 51);
             this.label3.TabIndex = 13;
             this.label3.Text = "»";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.numericUpDown1.Location = new System.Drawing.Point(300, 199);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             15,
@@ -164,7 +181,7 @@
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(63, 29);
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 39);
             this.numericUpDown1.TabIndex = 14;
             this.numericUpDown1.Value = new decimal(new int[] {
             10,
@@ -173,12 +190,108 @@
             0});
             this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.applicationToolStripMenuItem,
+            this.cSSToolStripMenuItem,
+            this.contactToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(705, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // applicationToolStripMenuItem
+            // 
+            this.applicationToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.alwaysOnTopToolStripMenuItem});
+            this.applicationToolStripMenuItem.Name = "applicationToolStripMenuItem";
+            this.applicationToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.applicationToolStripMenuItem.Text = "Application";
+            // 
+            // alwaysOnTopToolStripMenuItem
+            // 
+            this.alwaysOnTopToolStripMenuItem.Name = "alwaysOnTopToolStripMenuItem";
+            this.alwaysOnTopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.alwaysOnTopToolStripMenuItem.Text = "Always on Top";
+            this.alwaysOnTopToolStripMenuItem.Click += new System.EventHandler(this.alwaysOnTopToolStripMenuItem_Click);
+            // 
+            // cSSToolStripMenuItem
+            // 
+            this.cSSToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyColorCodeToolStripMenuItem,
+            this.copyFullCSSToolStripMenuItem1});
+            this.cSSToolStripMenuItem.Name = "cSSToolStripMenuItem";
+            this.cSSToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.cSSToolStripMenuItem.Text = "CSS";
+            // 
+            // copyColorCodeToolStripMenuItem
+            // 
+            this.copyColorCodeToolStripMenuItem.Name = "copyColorCodeToolStripMenuItem";
+            this.copyColorCodeToolStripMenuItem.Size = new System.Drawing.Size(165, 22);
+            this.copyColorCodeToolStripMenuItem.Text = "Copy Color Code";
+            this.copyColorCodeToolStripMenuItem.Click += new System.EventHandler(this.copyColorCodeToolStripMenuItem_Click);
+            // 
+            // copyFullCSSToolStripMenuItem1
+            // 
+            this.copyFullCSSToolStripMenuItem1.Name = "copyFullCSSToolStripMenuItem1";
+            this.copyFullCSSToolStripMenuItem1.Size = new System.Drawing.Size(165, 22);
+            this.copyFullCSSToolStripMenuItem1.Text = "Copy Full CSS";
+            this.copyFullCSSToolStripMenuItem1.Click += new System.EventHandler(this.copyFullCSSToolStripMenuItem1_Click);
+            // 
+            // contactToolStripMenuItem
+            // 
+            this.contactToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.projectSourceCodeToolStripMenuItem,
+            this.anytizerToolStripMenuItem});
+            this.contactToolStripMenuItem.Name = "contactToolStripMenuItem";
+            this.contactToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.contactToolStripMenuItem.Text = "Contact";
+            // 
+            // projectSourceCodeToolStripMenuItem
+            // 
+            this.projectSourceCodeToolStripMenuItem.Name = "projectSourceCodeToolStripMenuItem";
+            this.projectSourceCodeToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.projectSourceCodeToolStripMenuItem.Text = "Project Source Code";
+            this.projectSourceCodeToolStripMenuItem.Click += new System.EventHandler(this.projectSourceCodeToolStripMenuItem_Click);
+            // 
+            // anytizerToolStripMenuItem
+            // 
+            this.anytizerToolStripMenuItem.Name = "anytizerToolStripMenuItem";
+            this.anytizerToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.anytizerToolStripMenuItem.Text = "@anytizer";
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.DarkOrange;
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(10, 202);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(68, 83);
+            this.button6.TabIndex = 16;
+            this.button6.Text = "R";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(369, 199);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
             // master
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(703, 456);
+            this.ClientSize = new System.Drawing.Size(705, 455);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -191,17 +304,20 @@
             this.Controls.Add(this.trackBar3);
             this.Controls.Add(this.trackBar2);
             this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
             this.MaximizeBox = false;
             this.Name = "master";
             this.Text = "Color Master";
-            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,5 +336,16 @@
         private Label label2;
         private Label label3;
         private NumericUpDown numericUpDown1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem applicationToolStripMenuItem;
+        private ToolStripMenuItem alwaysOnTopToolStripMenuItem;
+        private ToolStripMenuItem contactToolStripMenuItem;
+        private ToolStripMenuItem projectSourceCodeToolStripMenuItem;
+        private ToolStripMenuItem anytizerToolStripMenuItem;
+        private Button button6;
+        private ToolStripMenuItem cSSToolStripMenuItem;
+        private ToolStripMenuItem copyColorCodeToolStripMenuItem;
+        private ToolStripMenuItem copyFullCSSToolStripMenuItem1;
+        private CheckBox checkBox1;
     }
 }
--- src/color-master/master.cs
+++ src/color-master/master.cs
@@ -1,17 +1,20 @@
+using System.Diagnostics;
 using System.Drawing;
+using System.Windows.Forms;
 using System.Windows.Forms.VisualStyles;
 
 namespace colormaster
 {
     public partial class master : Form
     {
-        private Button target;
+        private Button target_active;
+        //private Button target_inactive;
 
         public master()
         {
             InitializeComponent();
 
-            int steps = 1;
+            int steps = 2;
 
             this.trackBar1.Maximum = 255;
             this.trackBar1.Minimum = 0;
@@ -19,6 +22,7 @@ namespace colormaster
             this.trackBar1.LargeChange = steps * 16;
             this.trackBar1.SmallChange = steps * 4;
             this.trackBar1.TickStyle = TickStyle.None;
+            //this.trackBar1.BackColor = Color.Red;
 
             this.trackBar2.Maximum = 255;
             this.trackBar2.Minimum = 0;
@@ -26,6 +30,7 @@ namespace colormaster
             this.trackBar2.LargeChange = steps * 16;
             this.trackBar2.SmallChange = steps * 4;
             this.trackBar2.TickStyle = TickStyle.None;
+            //this.trackBar2.BackColor = Color.Green;
 
             this.trackBar3.Maximum = 255;
             this.trackBar3.Minimum = 0;
@@ -33,21 +38,31 @@ namespace colormaster
             this.trackBar3.LargeChange = steps * 16;
             this.trackBar3.SmallChange = steps * 4;
             this.trackBar3.TickStyle = TickStyle.None;
+            //this.trackBar3.BackColor = Color.Blue;
+
+            this.button4.BackColor = Color.Black;
+            this.button5.BackColor = Color.Black;
 
             this.numericUpDown1.Increment = 1;
             this.numericUpDown1.Maximum = 15;
-            this.numericUpDown1.Minimum = 0;
-            this.numericUpDown1.Value = 10;
+            this.numericUpDown1.Minimum = 1; // to start from 0 - 15
+            this.numericUpDown1.Value = 15;
             this.numericUpDown1.DecimalPlaces = 0;
             this.numericUpDown1.ThousandsSeparator = false;
             this.numericUpDown1.Hexadecimal = false;
 
-            this.Cursor = Cursors.Hand;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.target = this.button4;
+            this.button4.Text = string.Empty;
+            this.button5.Text = string.Empty;
+            //this.button6.Text = string.Empty; // Random()
+
+            this.Cursor = Cursors.Hand;
+            
+            this.target_active = this.button4;
+            //this.target_inactive = this.button5;
             this.adjust();
         }
 
@@ -66,12 +81,24 @@ namespace colormaster
             this.button2.Text = this.trackBar2.Value.ToString();
             this.button3.Text = this.trackBar3.Value.ToString();
 
-            this.target.BackColor = Color.FromArgb(this.trackBar1.Value, this.trackBar2.Value, this.trackBar3.Value);
-            this.target.ForeColor = Color.FromArgb(this.button4.BackColor.ToArgb() ^ 0XFFFFFF);
-            this.label1.Text = this.hexacolors();
+            this.target_active.BackColor = Color.FromArgb(this.trackBar1.Value, this.trackBar2.Value, this.trackBar3.Value);
+            this.target_active.ForeColor = Color.FromArgb(this.button4.BackColor.ToArgb() ^ 0XFFFFFF);
+            this.label1.Text = this.hex_colors();
+            
+            // optional
+            if(this.checkBox1.Checked)
+            {
+                this.label1.ForeColor = button5.BackColor;
+                this.label1.BackColor = button4.BackColor;
+            }
+            else
+            {
+                this.label1.ForeColor = Color.Black;
+                this.label1.BackColor = Color.Transparent;
+            }
         }
 
-        private string hexvalue(int integer)
+        private string hex(int integer)
         {
             string value = string.Format("{0:X}", integer.ToString("X"));
             if (value.Length == 1) value = "0" + value;
@@ -79,9 +106,9 @@ namespace colormaster
             return value;
         }
 
-        private string hexacolors()
+        private string hex_colors()
         {
-            string hexa = string.Format("#{0}{1}{2}", this.hexvalue(this.trackBar1.Value), this.hexvalue(this.trackBar2.Value), this.hexvalue(this.trackBar3.Value));
+            string hexa = string.Format("#{0}{1}{2}", this.hex(this.trackBar1.Value), this.hex(this.trackBar2.Value), this.hex(this.trackBar3.Value));
             return hexa;
         }
 
@@ -107,14 +134,16 @@ namespace colormaster
 
         private void button4_Click(object sender, EventArgs e)
         {
-            this.target = this.button4;
+            this.target_active = this.button4;
             this.update_sliders(this.button4.BackColor);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            this.target = this.button5;
+            this.target_active = this.button5;
             this.update_sliders(this.button5.BackColor);
+
+            this.target_active.BackColor = this.button5.BackColor;
         }
 
         private void update_sliders(Color color)
@@ -128,23 +157,23 @@ namespace colormaster
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.trackBar1.Value = this.get_random_hex();
+            this.trackBar1.Value = this.get_random_color_value();
             this.adjust();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.trackBar2.Value = this.get_random_hex();
+            this.trackBar2.Value = this.get_random_color_value();
             this.adjust();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            this.trackBar3.Value = this.get_random_hex();
+            this.trackBar3.Value = this.get_random_color_value();
             this.adjust();
         }
 
-        private int get_random_hex()
+        private int get_random_color_value()
         {
             int value = new Random().Next(0, 255);
             return value;
@@ -186,6 +215,67 @@ namespace colormaster
         {
            if(this.numericUpDown1.Value >= 15) this.numericUpDown1.Value = 15;
            if(this.numericUpDown1.Value <= 0) this.numericUpDown1.Value = 0;
+        }
+
+        private void alwaysOnTopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alwaysOnTopToolStripMenuItem.Checked = !alwaysOnTopToolStripMenuItem.Checked;
+            this.TopMost = alwaysOnTopToolStripMenuItem.Checked;
+        }
+
+
+        private string bgcolor()
+        {
+            string hexa = string.Format("#{0}{1}{2}", this.hex(this.button4.BackColor.R), this.hex(this.button4.BackColor.G), this.hex(this.button4.BackColor.B));
+            return hexa;
+        }
+
+        private void projectSourceCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string url = "https://github.com/anytizer/color-master.cs/";
+            try
+            {
+                Process opener = new Process();
+                opener.StartInfo.UseShellExecute = true;
+                opener.StartInfo.FileName = url;
+                opener.Start();
+            } catch (Exception ex)
+            {
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // randomize colors
+            int bk_red = this.get_random_color_value();
+            int bk_green = this.get_random_color_value();
+            int bk_blue = this.get_random_color_value();
+
+            this.trackBar1.Value = bk_red;
+            this.trackBar2.Value = bk_green;
+            this.trackBar3.Value = bk_blue;
+
+            // inactive one; if background is active, apply to foreground
+            //int fc_red= this.get_random_color_value();
+            //int fc_green = this.get_random_color_value();
+            //int fc_blue = this.get_random_color_value();
+
+            this.adjust();
+        }
+
+        private void copyColorCodeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.label1.Text);
+        }
+
+        private void copyFullCSSToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(string.Format(".element {{ color: {0}; background-color: {1}; }}", this.label1.Text, this.bgcolor()));
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.adjust();
         }
     }
 }
